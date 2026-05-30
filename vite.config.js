b63fe8e5d9ee8622426d4cb5755801ca1cb3201d
anthonywong0206-commits <anthony.wong0206@gmail.com>
2026-05-30 11:02:30 +0800

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        cream: '#FFF7E8',
+        wood: '#E8C6A1',
+        orange: '#F97316',
+        sage: '#8FAE8B',
+        ink: '#30251E'
+      },
+      boxShadow: {
+        soft: '0 24px 70px rgba(72, 45, 22, .14)'
+      }
+    }
+  },
+  plugins: []
+}
